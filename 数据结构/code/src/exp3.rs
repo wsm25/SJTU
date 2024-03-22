10e4e61235de5e251cf08200d43f682e6da55071
wsm25@honor <wsm_25@qq.com>
2024-03-22 13:17:11 +0800

    Experiment 3

--- 数据结构/code/src/exp3.rs
+++ 数据结构/code/src/exp3.rs
@@ -1,117 +1,143 @@
 #[cfg(test)]
 mod test{
     #[test]
-    fn test_triqueue(){
-        use super::triqueue::*;
+    fn triqueue(){
+        use super::super::exp3_triqueue::*;
+        // my test
         let mut q = TriQueue::new();
         q.push_front(1);
-        q.push_front(2);
-        assert_eq!(q.into_vec(), vec![2,1]);
+        q.push_back(2);
+        q.push_middle(3);
+        q.push_middle(4);
+        assert_eq!(q.into_vec(), vec![1,4,3,2]);
+        assert_eq!(q.pop_back().unwrap(), 2);
+        assert_eq!(q.pop_middle().unwrap(), 4);
+        assert_eq!(q.pop_middle().unwrap(), 1);
+        assert_eq!(q.pop_front().unwrap(), 3);
+        assert_eq!(q.pop_back(), None);
+        q.push_middle(1);
+        assert_eq!(q.pop_back().unwrap(), 1);
+        // test 1
+        let mut q = TriQueue::new();
+        q.push_front(13);
+        q.push_back(21);
+        q.push_middle(5);
+        q.pop_back();
+        q.pop_middle();
+        assert_eq!(q.into_vec(), vec![5]);
+        // test 2
+        q.pop_middle();
+        assert_eq!(q.into_vec(), vec![]);
+    }
+
+    #[test]
+    fn lunch(){
+        use super::lunch::*;
+        assert_eq!(unsatisfy(
+            &[1, 2, 3, 0, 0, 1, 2, 2, 2], 
+            &[1, 0, 1, 2, 0, 2, 2, 3, 2, 3, 1]
+        ), 0);
+
+        assert_eq!(unsatisfy(
+            &[0,2,3,0,2,3,1,3,2,3,3,3,1,0,1,0,2,2,3,1], 
+            &[1,1,3,0,2,2,0,0,1,3,0,3,2,3,2,2,3,0,3,3]
+        ), 3);
+
+    }
+
+    #[test]
+    fn stack_queue(){
+        use super::stack_queue::*;
+        use Type::*;
+        use Operation::*;
+
+        assert_eq!(type_of(&[
+            Push(1), Push(2), Push(3),
+            Pop(3),  Pop(2),  Pop(1),
+        ]), Stack);
+
+        assert_eq!(type_of(&[
+            Push(1), Push(2), Pop(1), Pop(2),
+        ]), Queue);
+
+        assert_eq!(type_of(&[
+            Push(5), Push(7), Push(7), Push(3),
+            Pop(3),  Pop(5),  Pop(7),  Pop(7),
+        ]), Error);
     }
 }
 
-mod triqueue{
-    use std::marker::PhantomData;
-    use std::alloc::{alloc, dealloc, Layout};
-    struct Node<T>{
-        left: *mut Self, // never null when inside dlist
-        right: *mut Self,
-        value: T,
+mod lunch{
+    pub fn unsatisfy(students: &[u8], lunch: &[u8])->usize{
+        let mut count=[0;10];
+        for i in students{
+            let i = *i as usize;
+            if i>=10 {panic!("type should < 10, got {i}")}
+            count[i]+=1;
+        }
+        for i in lunch{
+            let i = *i as usize;
+            if i>=10 {panic!("type should < 10, got {i}")}
+            if count[i]==0 {break;}
+            count[i]-=1;
+        }
+        let mut u=0;
+        for i in count {u+=i;}
+        u
     }
-    pub struct TriQueue<T>{
-        head: *mut Node<T>,
-        tail: *mut Node<T>,
-        middle: *mut Node<T>,
-        len: usize,
-        _phantom: PhantomData<T>,
+}
+
+mod stack_queue{
+    pub enum Operation{
+        Push(i32),
+        Pop(i32),
     }
-    impl<T> Node<T>{
-        const LAYOUT:Layout = Layout::new::<Self>();
-        unsafe fn new()->*mut Self{
-            alloc(Self::LAYOUT) as *mut Self
-        }
-        unsafe fn with_beside(left: *mut Self, right: *mut Self)->*mut Self{
-            let p = Self::new();
-            (*p).left=left;
-            (*p).right=right;
-            (*left).right=p;
-            (*right).left=p;
-            p
-        }
-        fn from(value:T, left: *mut Self, right: *mut Self)->*mut Self{
-            unsafe{
-                let p = Self::with_beside(left, right);
-                (*p).value=value;
-                p
-            }
-        }
+    use Operation::*;
 
-        unsafe fn delete(&mut self){
-            dealloc(self as *mut Self as *mut u8, Self::LAYOUT);
-        }
-    }
-
-    impl<T: Clone> TriQueue<T>{
-        pub fn new()->Self{unsafe{
-            let head = Node::new();
-            let tail = Node::new();
-            (*head).left=0 as *mut Node<T>;
-            (*head).right=tail;
-            (*tail).left=head;
-            (*head).right=0 as *mut Node<T>;
-            let middle = head; // head when empty, compatative to push_back and push_middle
-            TriQueue{
-                head, tail, middle, len:0, _phantom:PhantomData
-            }
-        }}
-
-        pub fn push_front(&mut self, x:T){unsafe{
-            let l = self.head;
-            let r = (*l).right;
-            let p = Node::from(x, l, r);
-            if self.len==0{
-                self.middle=p;
-                self.len=1;
-                return;
-            }
-            self.len+=1;
-            if (self.len&1)==0{
-                self.middle=(*self.middle).left;
-            }
-        }}
-
-        pub fn push_back(&mut self, x:T){unsafe{
-            let r = self.tail;
-            let l = (*r).left;
-            let _p = Node::from(x, l, r);
-            self.len+=1;
-            if (self.len&1)!=0{
-                self.middle=(*self.middle).right;
-            }
-        }}
-
-        pub fn push_middle(&mut self, x:T){unsafe{
-            let l = self.middle; // insert right of middle
-            let r =(*l).right;
-            let p = Node::from(x, l, r);
-            if (self.len&1)==0{ 
-                self.middle=p;
-            }
-            self.len+=1;
-        }}
-
-
-        pub fn into_vec(&self)->Vec<T>{
-            let mut v=Vec::new();
-            unsafe{
-                let mut p=(*self.head).right;
-                while p!=self.tail{
-                    v.push((*p).value.clone());
-                    p=(*p).right;
+    use std::collections::VecDeque;
+    fn is_queue(l: &[Operation])->bool{
+        let mut q=VecDeque::new();
+        for op in l{
+            match *op{
+            Push(x)=>q.push_back(x),
+            Pop(x) =>{
+                match q.pop_front(){
+                Some(y) => if x!=y {return false},
+                None => return false
                 }
-            }
-            v
+            }}
         }
-
+        true
+    }
+    
+    fn is_stack(l: &[Operation])->bool{
+        let mut q=VecDeque::new();
+        for op in l{
+            match *op{
+            Push(x)=>q.push_back(x),
+            Pop(x) =>{
+                match q.pop_back(){
+                Some(y) => if x!=y {return false},
+                None => return false
+                }
+            }}
+        }
+        true
+    }
+    #[derive(PartialEq, Debug)]
+    pub enum Type{Both, Stack, Queue, Error}
+    use Type::*;
+    pub fn type_of(l: &[Operation])->Type{
+        let (queue, stack) = (is_queue(l), is_stack(l));
+        match queue{
+            true=>match stack{
+                true=>Both,
+                false=>Queue,
+            },
+            false=>match stack{
+                true=>Stack,
+                false=>Error,
+            }
+        }
     }
 }
---
+++ 数据结构/code/src/exp3_triqueue.rs
@@ -0,0 +1,183 @@
+use std::marker::PhantomData;
+use std::alloc::{alloc, dealloc, Layout};
+struct Node<T>{
+    left: *mut Self, // never null when inside dlist
+    right: *mut Self,
+    value: T,
+}
+pub struct TriQueue<T>{
+    head: *mut Node<T>,
+    tail: *mut Node<T>,
+    middle: *mut Node<T>,
+    len: usize,
+    _phantom: PhantomData<T>,
+}
+
+impl<T> Node<T>{
+    unsafe fn new()->*mut Self{
+        alloc(Layout::new::<Self>()) as *mut Self
+    }
+    unsafe fn with_beside(left: *mut Self, right: *mut Self)->*mut Self{
+        let p = Self::new();
+        Self::connect(left, p);
+        Self::connect(p,right);
+        p
+    }
+    fn from(value:T, left: *mut Self, right: *mut Self)->*mut Self{
+        unsafe{
+            let p = Self::with_beside(left, right);
+            (*p).value=value;
+            p
+        }
+    }
+
+    unsafe fn read_value(p: *mut Self)->T{
+        unsafe{(&(*p).value as *const T).read()}
+    }
+
+    unsafe fn connect(left: *mut Self, right: *mut Self){
+        (*left).right=right;
+        (*right).left=left;
+    }
+
+    unsafe fn delete(p: *mut Self){
+        dealloc(p as *mut Self as *mut u8, Layout::new::<Self>());
+    }
+}
+
+impl<T> TriQueue<T>{
+    pub fn new()->Self{unsafe{
+        let head = Node::new();
+        let tail = Node::new();
+        Node::connect(head, tail);
+        let middle = head; // head when empty, compatative to push_back and push_middle
+        #[cfg(debug_assertions)] // debug for sigsegv
+        {
+            (*head).left=0 as *mut Node<T>;
+            (*tail).right=0 as *mut Node<T>;
+        }
+        TriQueue{
+            head, tail, middle, len:0, _phantom:PhantomData
+        }
+    }}
+
+    pub fn push_front(&mut self, x:T){unsafe{
+        let p = Node::from(x, 
+            self.head, 
+            (*self.head).right
+        );
+        if self.len==0{ // empty
+            self.middle=p;
+            self.len=1;
+            return;
+        }
+        if (self.len&1)!=0{
+            self.middle=(*self.middle).left;
+        }
+        self.len+=1;
+        
+    }}
+
+    pub fn push_back(&mut self, x:T){unsafe{
+        let _p = Node::from(x, (*self.tail).left, self.tail);
+        if (self.len&1)==0{
+            self.middle=(*self.middle).right;
+        }
+        self.len+=1;
+    }}
+
+    pub fn push_middle(&mut self, x:T){unsafe{
+        self.middle = 
+            if (self.len&1)==0{ // insert right of middle
+                Node::from(x, 
+                    self.middle, 
+                    (*self.middle).right
+                )
+            } else { // left
+                Node::from(x,
+                    (*self.middle).left, 
+                    self.middle
+                )
+            };
+        self.len+=1;
+    }}
+
+    pub fn pop_front(&mut self)->Option<T>{unsafe{
+        if self.len==0 {
+            return None;
+        }
+        self.len-=1;
+        if self.len==0 { // empty
+            self.middle=self.head;
+        }
+        // non-empty
+        if (self.len&1)!=0{
+            self.middle=(*self.middle).right;
+        }
+        let p = (*self.head).right;
+        let x = Node::read_value(p);
+        Node::connect(self.head, (*p).right);
+        Node::delete(p);
+        
+        Some(x)
+    }}
+
+    pub fn pop_back(&mut self)->Option<T>{unsafe{
+        if self.len==0 {
+            return None;
+        }
+        self.len-=1;
+        // non-empty
+        if (self.len&1)==0{
+            self.middle=(*self.middle).left;
+        }
+        let p = (*self.tail).left;
+        let x = Node::read_value(p);
+        Node::connect((*p).left, self.tail);
+        Node::delete(p);
+        Some(x)
+    }}
+
+    pub fn pop_middle(&mut self)->Option<T>{unsafe{
+        if self.len==0 {
+            return None;
+        }
+        self.len-=1;
+        let p = self.middle;
+        let x = Node::read_value(p);
+        self.middle = 
+            if (self.len&1)==0{ 
+                (*p).left
+            } else {
+                (*p).right
+            };
+        Node::connect((*p).left, (*p).right);
+        Node::delete(p);
+        Some(x)
+    }}
+
+    pub fn into_vec(&self)->Vec<T>{
+        let mut v=Vec::new();
+        unsafe{
+            let mut p=(*self.head).right;
+            while p!=self.tail{
+                v.push(Node::read_value(p));
+                p=(*p).right;
+            }
+        }
+        v
+    }
+}
+
+impl<T> Drop for TriQueue<T>{
+    fn drop(&mut self) {unsafe{
+        let mut p=(*self.head).right;
+        while p!=self.tail{
+            let x = (*p).right;
+            Node::delete(p);
+            p=x;
+        }
+        Node::delete(self.head);
+        Node::delete(self.tail);
+    }}
+}
