6a85c9f150cd37d63f8f27c6e90d9754bc29d022
wsm25@honor <wsm_25@qq.com>
2024-04-04 00:50:48 +0800

    preorder iterator for linked binary tree

--- 数据结构/structures/src/binary_tree.rs
+++ 数据结构/structures/src/binary_tree.rs
@@ -1,7 +1,5 @@
-/// # Binary Tree
-///
-/// Implement with link structure.
-/// 
+//! # Binary Tree
+//! Implement with link structure.
 
 use std::{boxed::Box, marker::PhantomData};
 pub struct Node<T>{
@@ -19,22 +17,47 @@ impl<T> Node<T>{
     ->Box<Node<T>>{
         Box::new(Node { left: Some(left), right: Some(right), value })
     }
+
+    pub fn into_preiter<'a>(&'a mut self)->PreIter<'a, T>{
+        let mut stack=Vec::with_capacity(4);
+        stack.push((self as *mut Self, PreIterStatus::Pre));
+        PreIter{stack, phantom: PhantomData}
+    }
 }
 
+enum PreIterStatus{Pre, Post, End}
 pub struct PreIter<'a, T>{
-    stack: Vec<(Box<Node<T>>, usize)>,
-    life: PhantomData<&'a mut T>,
+    stack: Vec<(*mut Node<T>, PreIterStatus)>,
+    phantom: PhantomData<&'a mut T>
 }
 impl<'a, T> Iterator for PreIter<'a, T>{
     type Item = &'a mut T;
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some((node, count))=self.stack.last_mut(){
-            let mut p = Some(node);
-            while let Some(node) = p{
-                
+    fn next(&mut self) -> Option<Self::Item> {unsafe{
+        use PreIterStatus::*;
+        match self.stack.pop(){
+        None=>None,
+        Some((node, status))=>{
+            match status{
+            Pre=>{
+                self.stack.push((node, Post));
+                match &mut (*node).left{
+                    Some(n)=>self.stack.push((&mut **n, Pre)),
+                    None=>()
+                }
+                Some(&mut (*node).value)
+            },
+            Post=>{
+                match &mut (*node).right{
+                    Some(n)=>{
+                        self.stack.push((node, End));
+                        self.stack.push((&mut **n, Pre));
+                        self.next()
+                    },
+                    None=>self.next()
+                }
+            },
+            End=>self.next()
             }
-        } else {
-            None
-        }
-    }
+        }}
+    }}
 }
